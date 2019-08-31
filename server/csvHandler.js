1d9787441636bb4cdb12d52a79accb6b123f1b3b
a3rtgm <info@artagam.de>
2019-08-31 19:35:17 +0200

    filter csv, create json, res data obj to client

--- server/csvHandler.js
+++ server/csvHandler.js
@@ -1,9 +1,28 @@
 const d3 = require('../node_modules/d3');
+const mat = require('./material');
+const dsv = d3.dsvFormat(";");
 
-function csvToJson (csv) {
-    d3.dsv(';', csv, (d) => {
-        console.log(d);
+async function csvToJson (csv) {
+    var catId = "",
+        catName = "";
+
+    return dsv.parse(csv, (d) => {
+        return filterData(d);
     });
+}
+
+function filterData (d) {
+    if (d.ArbPlatz === "INV") {
+        catId = d["Material P"];
+        catName = d["Objektkurztext"];
+    } else {
+        if (d.SchGut === "X") {
+            if (d.MArt === "ROH" || d.MArt === "HALB") {
+                return new mat.Item(d, catId, catName);
+            }
+        }
+    }
+    return;
 }
 
 module.exports = { csvToJson };
--- server/upload.js
+++ server/upload.js
@@ -9,31 +9,42 @@ const reader = new FileReader();
 
 const uploadDir = "./user-upload/";
 
-module.exports = function upload(req, res) {
+module.exports = async function upload(req, res) {
     let form = new IncomingForm();
 
     form.on('file', (field, file) => {
-        res.send(file);
-
         // Parse & Save to Disk
         reader.readAsArrayBuffer(file)
         reader.addEventListener('load', (evt) => {
             const view = new Uint8Array(reader.result);
 
             // retrieve data as {json: obj, csv: string, date: string, uploadDate: Date}
-            const newDatum = parser(reader.result);
+            let newDatum = parser(reader.result);
 
             // save files to server dir
             fs.writeFile(path.join(uploadDir, file.name), view);
             fs.writeFile(path.join(uploadDir, `${file.name}.csv`), newDatum.csv);
 
-            csvHandler.csvToJson(newDatum.csv);
+            // send csv to csvHandler and wait for resolution
+            // return the new data-object
+            // send the json back to the client as response
+            addJson(newDatum);
         });
     });
 
     form.on('end', () => {
-        res.json();
+        // res.json();
     });
 
     form.parse(req);
 };
+
+async function addJson (datum) {
+    // wait for the d3.dsv-handler to filter and convert the csv-string
+    let promise = new Promise ((res, rej) => {
+        res(csvHandler.csvToJson(datum.csv));
+    });
+    datum.json = await promise;
+
+    return datum;
+}
