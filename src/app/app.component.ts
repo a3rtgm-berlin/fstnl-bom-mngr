979e70684cb89ff91095a74f080211bfee845e4d
a3rtgm <info@artagam.de>
2019-09-28 18:03:14 +0200

    update - include jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MaterialList } from './materialListModel';
 import { ModalService } from './services/modal/modal.service';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'BOM Supply Manager';
   modalService: ModalService;
 
   constructor(modalService: ModalService) {
     this.modalService = modalService;
+  }
+
+  ngOnInit(): void {
   }
 
   removeModal() {
