232bb6785a5793c2dcdfa9e4bff77baf4a494fc5
bertramhillmann <bertram.hillmann@artagam.de>
2019-10-06 14:18:36 +0200

    Bei Robert zuhaus

--- src/app/compare-orders/comparison-list-view/comparison-list-view.component.ts
+++ src/app/compare-orders/comparison-list-view/comparison-list-view.component.ts
@@ -33,50 +33,53 @@ export class ComparisonListViewComponent implements OnInit, OnChanges {
   addSort(cat, evt) {
    if($(evt.target).siblings().hasClass("filter")) {
       if($(evt.target).siblings().hasClass("filter-2")) {
+        let cat1 = $(evt.target).siblings(".filter").data("sort");
+        let cat2 = $(evt.target).siblings(".filter-2").data("sort");
         $(evt.target).addClass("filter-3");
+        this.bom$.sort(this.dynamicSort(cat1, cat2, cat));
+
       } else {
         let cat1 = $(evt.target).siblings(".filter").data("sort");
         $(evt.target).addClass("filter-2");
-        this.bom$.sort(this.dynamicSortMultiple(cat1, cat));
-        
+        $(evt.target).data("sort", cat);
+        this.bom$.sort(this.dynamicSort(cat1, cat));
+
       }
+    } else if ($(evt.target).hasClass("filter") || $(evt.target).hasClass("filter-2") || $(evt.target).hasClass("filter-3")) {
+        if ($(evt.target).hasClass("filter")) {
+          $(evt.target).removeClass("filter");
+          $(evt.target).siblings(".filter-2").addClass("filter").removeClass("filter-2");
+          $(evt.target).siblings(".filter-3").addClass("filter-2").removeClass("filter-3");
+          let cat1 = $(evt.target).siblings("filter").data("sort");
+          let cat = $(evt.target).siblings("filter-2").data("sort");
+          this.bom$.sort(this.dynamicSort(cat1, cat));
+        } if ($(evt.target).hasClass("filter-2")) {
+            $(evt.target).removeClass("filter-2");
+            $(evt.target).siblings(".filter-3").addClass("filter-2").removeClass("filter-3");
+            let cat1 = $(evt.target).siblings("filter").data("sort");
+            let cat = $(evt.target).siblings("filter-2").data("sort");
+            this.bom$.sort(this.dynamicSort(cat1, cat));
+        } if($(evt.target).hasClass("filter-3")) {
+            $(evt.target).removeClass("filter-3");
+            let cat1 = $(evt.target).siblings("filter").data("sort");
+            let cat = $(evt.target).siblings("filter-2").data("sort");
+            this.bom$.sort(this.dynamicSort(cat1, cat));
+        }
+
     } else {
       $(evt.target).addClass("filter");
       $(evt.target).data("sort", cat);
       this.bom$.sort(this.dynamicSort(cat));
-    }
+    } 
   }
 
-  dynamicSort(property) {
-    var sortOrder = 1;
-    if(property[0] === "-") {
-        sortOrder = -1;
-        property = property.substr(1);
-    }
-    return function (a,b) {
-        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-        return result * sortOrder;
-    }
-}
-
-dynamicSortMultiple() {
-  /*
-   * save the arguments object as it will be overwritten
-   * note that arguments object is an array-like object
-   * consisting of the names of the properties to sort by
-   */
-  var props = arguments;
-  return function (obj1, obj2) {
-      var i = 0, result = 0, numberOfProperties = props.length;
-      /* try getting a different result from 0 (equal)
-       * as long as we have extra properties to compare
-       */
-      while(result === 0 && i < numberOfProperties) {
-        result = this.dynamicSort(props[i])(obj1, obj2);
-          i++;
-      }
+  dynamicSort(...props) {
+    var args = arguments;
+  
+     return function (a,b) {
+      var result = a[args[0]] < b[args[0]] ? -1 : a[args[0]] > b[args[0]] ? 1 : 0 || a[args[1]] < b[args[1]] ? -1 : a[args[1]] > b[args[1]] ? 1 : 0 || a[args[2]] < b[args[2]] ? -1 : a[args[2]] > b[args[2]] ? 1 : 0;
       return result;
+    }
   }
-}
 
 }
