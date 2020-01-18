00ee1947b19896c0c1d4b3c2cb6ecdd0ada66cf2
bertramhillmann <bertram.hillmann@artagam.de>
2020-01-18 18:53:40 +0100

    leckomo

--- server/authorization.js
+++ server/authorization.js
@@ -3,6 +3,7 @@ const verification = require('./models/verification');
 
 const guard = function (req, res, callback, privateKey = verification.privateKey, verifyOptions = verification.verifyOptions)  {
     if (req.headers.authorization) {
+        console.log(req.headers.authorization);
         jwt.verify(req.headers.authorization, privateKey, verifyOptions, (err, decoded) => {
             if (err) {
                 return res.sendStatus(401);
