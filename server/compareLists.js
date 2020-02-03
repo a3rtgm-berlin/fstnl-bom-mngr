30fb9b185281986cf4cc6d0c0a69865fcbddcf45
a3rtgm-ds <daniel.schulz@hcu-hamburg.de>
2020-02-03 15:19:02 +0100

    fix stuff!

--- server/compareLists.js
+++ server/compareLists.js
@@ -15,12 +15,12 @@ module.exports = class Comparison {
         
         this.lastList = {
             id: sortedLists[0].id,
-            json: sortedLists[0].json
+            json: JSON.parse(JSON.stringify(sortedLists[0].json))
             // json: this.concatModulesAtStation(sortedLists[0].json)
         };
         this.currentList = {
             id: sortedLists[1].id,
-            json: sortedLists[1].json
+            json: JSON.parse(JSON.stringify(sortedLists[1].json))
             // json: this.concatModulesAtStation(sortedLists[1].json)
         };
 
